e4623fd04170fffd9319b13f3a613cdb06db7116
pavlosZakkas <pavloszk93@gmail.com>
2022-02-09 19:14:48 +0200

    Implement SARSA algorithm
    - Start from random state
    - Make a step in the environment by selecting the next action
    according to action selection policy
    - Retrieve next state and reward
    - Sample the next action to be taken from the returned next state
    - Update Q values table according to env's feedback and the selected
    next action
    - Continuw from already defined next state and next action.

--- SARSA.py
+++ SARSA.py
@@ -8,6 +8,8 @@ By Thomas Moerland
 """
 
 import numpy as np
+
+from ActionSelectionPolicy import ActionSelectionPolicy, AnnealingEGreedyPolicy
 from Environment import StochasticWindyGridworld
 from Helper import softmax, argmax
 
@@ -18,45 +20,52 @@ class SarsaAgent:
         self.n_actions = n_actions
         self.learning_rate = learning_rate
         self.gamma = gamma
-        self.Q_sa = np.zeros((n_states,n_actions))
+        self.Q_sa = np.zeros((n_states, n_actions))
         
-    def select_action(self, s, policy='egreedy', epsilon=None, temp=None):
+    def select_action(self, state, policy: ActionSelectionPolicy):
+        return policy.select_action_from(state, self.Q_sa, self.n_actions)
         
-        if policy == 'egreedy':
-            if epsilon is None:
-                raise KeyError("Provide an epsilon")
-                
-            # TO DO: Add own code
-            a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
-            
-                
-        elif policy == 'softmax':
-            if temp is None:
-                raise KeyError("Provide a temperature")
-                
-            # TO DO: Add own code
-            a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
-            
-        return a
-        
-    def update(self,s,a,r,s_next,a_next,done):
-        # TO DO: Add own code
-        pass
-        
-def sarsa(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, temp=None, plot=True):
+    def update(self, state, action, reward, next_state, next_action, done):
+        back_up_estimate = reward + self.gamma * self.Q_sa[next_state][next_action]
+        self.Q_sa[state][action] += self.learning_rate * (back_up_estimate - self.Q_sa[state][action])
+
+    def a_random_state(self):
+        return np.random.randint(0, self.n_states)
+
+def sarsa(
+    n_timesteps,
+    learning_rate,
+    gamma,
+    policy: ActionSelectionPolicy,
+    plot=True
+):
     ''' runs a single repetition of SARSA
     Return: rewards, a vector with the observed rewards at each timestep ''' 
     
     env = StochasticWindyGridworld(initialize_model=False)
-    pi = SarsaAgent(env.n_states, env.n_actions, learning_rate, gamma)
+    agent = SarsaAgent(env.n_states, env.n_actions, learning_rate, gamma)
     rewards = []
 
-    # TO DO: Write your SARSA algorithm here!
-    
-    # if plot:
-    #    env.render(Q_sa=pi.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during SARSA execution
+    state = agent.a_random_state()
+    action = agent.select_action(state, policy)
+    env.set_location_from(state)
 
-    return rewards 
+    for timestep in range(n_timesteps):
+        next_state, reward, done = env.step(action)
+        rewards.append(reward)
+        next_action = agent.select_action(next_state, policy)
+        agent.update(state, action, reward, next_state, next_action, done)
+
+        state = next_state
+        action = next_action
+
+        if plot:
+            env.render(Q_sa=agent.Q_sa, plot_optimal_policy=True, step_pause=0.1)
+
+        if done:
+            break
+
+    return rewards
 
 
 def test():
@@ -65,14 +74,25 @@ def test():
     learning_rate = 0.1
 
     # Exploration
-    policy = 'egreedy' # 'egreedy' or 'softmax' 
-    epsilon = 0.1
-    temp = 1.0
+    # policy = EGreedyPolicy(epsilon=0.1)
+    policy = AnnealingEGreedyPolicy(
+        timesteps=n_timesteps,
+        initial_epsilon=0.9,
+        final_epsilon=0.001,
+        steps_percentage=0.9
+    )
+    # policy = SoftMaxPolicy(temperature=1.0)
+    # policy = AnnealingSoftMaxPolicy(
+    #     timesteps=n_timesteps,
+    #     initial_temperature=10,
+    #     final_temperature=0.001,
+    #     steps_percentage=0.9
+    # )
     
     # Plotting parameters
     plot = True
 
-    rewards = sarsa(n_timesteps, learning_rate, gamma, policy, epsilon, temp, plot)
+    rewards = sarsa(n_timesteps, learning_rate, gamma, policy, plot)
     print("Obtained rewards: {}".format(rewards))        
     
 if __name__ == '__main__':
---
+++ tests/SARSA_spec.py
@@ -0,0 +1,250 @@
+import unittest
+from parameterized import parameterized
+import numpy as np
+import mock
+from mock import Mock
+
+from ActionSelectionPolicy import EGreedyPolicy, SoftMaxPolicy
+from SARSA import SarsaAgent, sarsa
+
+class SARSASpec(unittest.TestCase):
+  STATES = 3
+  ACTIONS = 3
+  GAMMA = 0.9
+  LEARNING_RATE = 0.1
+  EPSILON = 0.1
+  TEMPERATURE = 10.0
+  THRESHOLD = 0.001
+  TIMESTEPS = 1000
+
+  ACTION_1 = 0
+  ACTION_2 = 1
+  ACTION_3 = 2
+
+  STATE_1 = 0
+  STATE_2 = 1
+  STATE_3 = 2
+  STATE_VALUES_1 = [0, 3, 5]
+  STATE_VALUES_2 = [10, 11, 8]
+  STATE_VALUES_3 = [30, 21, 18]
+
+  SOFTMAX_DENOMINATOR_SUM = np.sum(np.exp([-5 / TEMPERATURE, -2 / TEMPERATURE, 0 / TEMPERATURE]))
+  SOFTMAX_STATE_VALUES_1 = [
+    np.exp(-5 / TEMPERATURE) / SOFTMAX_DENOMINATOR_SUM,
+    np.exp(-2 / TEMPERATURE) / SOFTMAX_DENOMINATOR_SUM,
+    np.exp(0 / TEMPERATURE) / SOFTMAX_DENOMINATOR_SUM,
+  ]
+
+  MOCKED_RANDOM_STATE = STATE_2
+  MOCKED_NEXT_STATE = STATE_1
+  MOCKED_REWARD = -1.0
+  MOCKED_UPDATE = 1.0
+  DONE = True
+  MOCKED_SELECTED_ACTION = ACTION_3
+  MOCKED_NEXT_ACTION = ACTION_3
+  STEPS_TO_REACH_GOAL = 10
+
+  @parameterized.expand([
+    (STATE_1, ACTION_3),
+    (STATE_2, ACTION_2),
+    (STATE_3, ACTION_1),
+  ])
+  @mock.patch('ActionSelectionPolicy.np')
+  def should_select_the_highest_valued_action_for_egreedy_policy_if_the_sampled_selection_probability_is_greater_than_epsilon(
+    self,
+    state,
+    action,
+    mocked_numpy
+  ):
+    # given
+    agent = SarsaAgent(self.STATES, self.ACTIONS, self.LEARNING_RATE, self.GAMMA)
+    agent.Q_sa = [
+      self.STATE_VALUES_1,
+      self.STATE_VALUES_2,
+      self.STATE_VALUES_3,
+    ]
+
+    mocked_numpy.random.uniform.return_value = self.EPSILON + 0.05
+    # when
+    selected_action = agent.select_action(state, EGreedyPolicy(self.EPSILON))
+
+    # then
+    self.assertEqual(selected_action, action)
+
+  @mock.patch('ActionSelectionPolicy.np')
+  def should_select_a_random_action_for_egreedy_policy_if_the_sampled_selection_probability_is_smaller_than_epsilon(
+    self,
+    mocked_numpy
+  ):
+    # given
+    agent = SarsaAgent(self.STATES, self.ACTIONS, self.LEARNING_RATE, self.GAMMA)
+    agent.Q_sa = [
+      self.STATE_VALUES_1,
+      self.STATE_VALUES_2,
+      self.STATE_VALUES_3,
+    ]
+
+    mocked_numpy.random.uniform.return_value = self.EPSILON - 0.05
+    mocked_numpy.random.randint.return_value = self.ACTION_2
+
+    # when
+    selected_action = agent.select_action(self.STATE_1, EGreedyPolicy(self.EPSILON))
+
+    # then
+    self.assertEqual(selected_action, self.ACTION_2)
+
+  @mock.patch('ActionSelectionPolicy.np')
+  def should_select_an_action_for_boltzmann_policy_based_on_softmax_of_actions(self, mocked_numpy):
+    # given
+    agent = SarsaAgent(self.STATES, self.ACTIONS, self.LEARNING_RATE, self.GAMMA)
+    agent.Q_sa = [
+      self.STATE_VALUES_1,
+      self.STATE_VALUES_2,
+      self.STATE_VALUES_3,
+    ]
+
+    def chosen_action(actions, size, p):
+      self.assertEqual(actions, range(self.ACTIONS))
+      self.assertEqual(size, 1)
+      self.assertEqual(list(p), list(self.SOFTMAX_STATE_VALUES_1))
+      return [self.ACTION_2]
+
+    mocked_numpy.random.choice.side_effect = chosen_action
+    # when
+    selected_action = agent.select_action(self.STATE_1, SoftMaxPolicy(self.TEMPERATURE))
+
+    # then
+    self.assertEqual(selected_action, self.ACTION_2)
+
+  def should_update_Q_table_based_on_one_step_sarsa_update(self):
+    # given
+    agent = SarsaAgent(self.STATES, self.ACTIONS, self.LEARNING_RATE, self.GAMMA)
+    agent.Q_sa = [
+      self.STATE_VALUES_1.copy(),
+      self.STATE_VALUES_2.copy(),
+      self.STATE_VALUES_3.copy(),
+    ]
+    reward = 10.0
+
+    # when
+    agent.update(self.STATE_1, self.ACTION_2, reward, self.STATE_2, self.ACTION_3, None)
+
+    # then
+    updated_Q_value = 3 + self.LEARNING_RATE * ((reward + self.GAMMA * self.STATE_VALUES_2[self.ACTION_3]) - 3)
+    assert agent.Q_sa == [
+      [0, updated_Q_value, 5],
+      self.STATE_VALUES_2,
+      self.STATE_VALUES_3
+    ]
+
+  @mock.patch('Environment.StochasticWindyGridworld')
+  @mock.patch('SARSA.SarsaAgent')
+  def should_update_Q_table_and_return_rewards_per_timestep_of_sarsa_algorithm(
+    self,
+    mocked_SarsaAgent,
+    mocked_windy_grid
+  ):
+    # given
+    mocked_env = self.a_mocked_env()
+    mocked_windy_grid.return_value = mocked_env
+    mocked_agent = self.a_mocked_agent()
+    mocked_SarsaAgent.return_value = mocked_agent
+
+    # when
+    rewards = sarsa(
+      self.TIMESTEPS,
+      self.LEARNING_RATE,
+      self.GAMMA,
+      policy=EGreedyPolicy(epsilon=self.EPSILON),
+      plot=False,
+    )
+
+    # then
+    expected_update_calls = [
+                              mock.call(
+                                self.MOCKED_RANDOM_STATE,
+                                self.MOCKED_SELECTED_ACTION,
+                                self.MOCKED_REWARD,
+                                self.MOCKED_NEXT_STATE,
+                                self.MOCKED_SELECTED_ACTION,
+                                not self.DONE
+                              )
+                            ] + (self.TIMESTEPS - 1) * [
+                              mock.call(
+                                self.MOCKED_NEXT_STATE,
+                                self.MOCKED_SELECTED_ACTION,
+                                self.MOCKED_REWARD,
+                                self.MOCKED_NEXT_STATE,
+                                self.MOCKED_NEXT_ACTION,
+                                not self.DONE
+                              )
+                            ]
+
+    self.assertEqual(mocked_agent.update.call_count, len(expected_update_calls))
+    mocked_agent.update.assert_has_calls(expected_update_calls)
+    np.testing.assert_array_equal(
+      mocked_agent.Q_sa,
+      [[0., 0., self.TIMESTEPS - 1], [0., 0., 1.], [0., 0., 0.]]
+    )
+
+    self.assertEqual(rewards, [self.MOCKED_REWARD] * self.TIMESTEPS)
+
+  @mock.patch('SARSA.SarsaAgent')
+  @mock.patch('SARSA.StochasticWindyGridworld')
+  def should_stop_if_goal_state_is_reached(self, mocked_windy_grid, mocked_SarsaAgent):
+    # given
+    mocked_env = self.a_mocked_env(steps_to_reach_goal=self.STEPS_TO_REACH_GOAL)
+    mocked_windy_grid.return_value = mocked_env
+    mocked_agent = self.a_mocked_agent()
+    mocked_SarsaAgent.return_value = mocked_agent
+
+    # when
+    rewards = sarsa(
+      self.TIMESTEPS,
+      self.LEARNING_RATE,
+      self.GAMMA,
+      policy=EGreedyPolicy(self.EPSILON),
+      plot=False,
+    )
+
+    # then
+    self.assertEqual(mocked_agent.update.call_count, self.STEPS_TO_REACH_GOAL)
+    self.assertEqual(rewards, [self.MOCKED_REWARD] * self.STEPS_TO_REACH_GOAL)
+
+  def a_mocked_env(self, steps_to_reach_goal=None):
+    def mocked_env_steps(steps_to_reach_goal):
+      for step in range(steps_to_reach_goal-1):
+        yield self.MOCKED_NEXT_STATE, self.MOCKED_REWARD, not self.DONE
+      yield self.MOCKED_NEXT_STATE, self.MOCKED_REWARD, self.DONE
+
+    env_steps_generator = mocked_env_steps(
+      steps_to_reach_goal if steps_to_reach_goal != None
+      else self.TIMESTEPS
+    )
+
+    def mocked_env_steps(action, **kwargs):
+      return env_steps_generator.__next__()
+
+    env = Mock()
+    env.n_states = self.STATES
+    env.n_actions = self.ACTIONS
+
+    env.set_location_from = Mock()
+    env.step = Mock(side_effect=mocked_env_steps)
+    return env
+
+  def a_mocked_agent(self):
+    agent = Mock()
+
+    agent.n_states = self.STATES
+    agent.n_actions = self.ACTIONS
+    agent.Q_sa = np.zeros((self.STATES, self.ACTIONS))
+
+    def mocked_update_agent_Q_sa(state, action, reward, next_state, next_action, done, **kwargs):
+      assert action == self.MOCKED_SELECTED_ACTION
+      agent.Q_sa[state][action] += self.MOCKED_UPDATE
+
+    agent.update.side_effect = mocked_update_agent_Q_sa
+    agent.a_random_state = Mock(return_value=self.MOCKED_RANDOM_STATE)
+    agent.select_action = Mock(return_value=self.MOCKED_SELECTED_ACTION)
+    return agent
